cf0dee6dd771db4a4fcdf6733d686a641e45acb2
MAKSYM <makfuuu@gmail.com>
2026-04-13 17:05:30 +0200

    update settings

--- petition/mainapp/models.py
+++ petition/mainapp/models.py
@@ -28,13 +28,14 @@ class Signature(models.Model):
 class Question(models.Model):
     STATUS_CHOICES = [
         ('pending', 'Auf Moderation'),
-        ('approved', 'Erlaubt'),
+        ('approved', 'Erklaubt'),
         ('rejected', 'Nicht erlaubt'),
     ]
     question = models.TextField()
     answer = models.TextField(blank=True, default='')
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
     date = models.DateTimeField(auto_now_add=True)
+    ip_address = models.GenericIPAddressField(null=True, blank=True)
 
     def __str__(self):
         return self.question[:50]
