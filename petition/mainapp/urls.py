669112cb7b871467d3b573d607f5311e4b5dbde5
MAKSYM <makfuuu@gmail.com>
2026-04-15 21:55:44 +0200

    update settings

--- petition/mainapp/urls.py
+++ petition/mainapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('reviews/all/', views.all_reviews, name='all_reviews'),
     path('faq/', views.faq, name='faq'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='imdex.html'), name='logout'),
     path('moderation/', views.moderation, name='moderation'),
     path('moderation/<int:question_id>/', views.answer_question, name='answer_question'),
     path('moderation/delete/<int:question_id>/', views.delete_question, name='delete_question'),
