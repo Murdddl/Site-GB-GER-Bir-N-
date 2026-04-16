2af9c0f0014d4faa5f92ac82e31280efdd2e0998
MAKSYM <makfuuu@gmail.com>
2026-04-16 18:35:08 +0200

    update settings

--- petition/mainapp/models.py
+++ petition/mainapp/models.py
@@ -42,3 +42,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question[:50]
     
+class Comment(models.Model):
+    user = models.CharField(max_length=100)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
