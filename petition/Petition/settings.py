a0f1ff2bfa6a39a324df0af4fd29e33f729764c5
MAKSYM <makfuuu@gmail.com>
2026-04-19 22:15:47 +0200

    update settings

--- petition/Petition/settings.py
+++ petition/Petition/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         #'PORT': os.getenv('DB_PORT'),
         
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/murdddl/db.sqlite3',
+        'NAME': '/home/Murdddl/db.sqlite3',
     }
 }
 
